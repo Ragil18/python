d70c1b84d591ccad387c04eb384355e5ab8fd534
Ragil18 <90504396+Ragil18@users.noreply.github.com>
2021-09-15 22:18:15 +0530

    Create finding most frequent word in a text.py

---
+++ finding most frequent word in a text.py
@@ -0,0 +1,25 @@
+def main():
+filename=raw_input("enter the file").strip()
+infile=open(filename,"r")
+wordcounts={}
+for line in infile:
+processLine(line.lower(),wordcounts)
+pairs=list(wordcounts.items())
+items=[[x,y] for (y,x) in pairs]
+items.sort()
+for i in range(len(items)-1,len(items)-11,-1):
+print(items[i][1]+"\t"+str(items[i][0]))
+def processLine(line,wordcounts):
+line=replacePunctuations(line)
+words=line.split()
+for word in words:
+if word in wordcounts:
+wordcounts[word]+=1
+else:
+wordcounts[word]=1
+def replacePunctuations(line):
+for ch in line:
+if ch in "~@#$%^&*()_-+=<>?/.,:;!{}[]''":
+line=line.replace(ch," ")
+return line
+main()
